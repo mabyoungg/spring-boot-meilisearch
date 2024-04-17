084da6c3ce3ad9fbb432210aa5236a1d21160fa6
mabyoungg <mabyoungg@gmail.com>
2024-04-17 20:04:36 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/springbootmeilisearch/SpringBootMeilisearchApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootmeilisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMeilisearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMeilisearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootmeilisearch/SpringBootMeilisearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootmeilisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMeilisearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
